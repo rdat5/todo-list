c305c175c0e79510013ab5512518f15aaa4a9153
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-25 21:07:48 -0700

    Create a class for Todo objects

---
+++ src/todo.js
@@ -0,0 +1,19 @@
+class Todo
+{
+    constructor(title = "Task name", desc = "Task description", dueDate = "1/1/1901", priority = "1")
+    {
+        this.title = title;
+        this.desc = desc;
+        this. dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    isDone = false;
+
+    toggleIsDone()
+    {
+        this.isDone = !this.isDone;
+    }
+}
+
+export { Todo };
