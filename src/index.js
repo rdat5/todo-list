d8d39ed8832422ac66608e9b0683e3fac3733ef6
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-26 20:59:14 -0700

    Create default project list on startup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { initializeUI } from './ui_load.js';
+import { UserProjects } from './userProjects.js';
+import { Project } from './project.js';
+import { Todo } from './todo.js';
 console.log("Testing...");
 
 initializeUI();
+
+// Add initial default project with default todo
+const userProjects = new UserProjects();
+userProjects.addProject(new Project());
+userProjects.projects[0].addTodo(new Todo());
