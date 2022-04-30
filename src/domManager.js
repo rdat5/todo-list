998dc7a90ccc42f0abbed327872ffa63314935b8
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-29 22:17:48 -0700

    Make todos render from a generated element

--- src/domManager.js
+++ src/domManager.js
@@ -27,6 +27,35 @@ class DomManager
         pageWrap.appendChild(footerElem);
     }
 
+    generateTodoCard(todo)
+    {
+        // Todo container
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container');
+        
+        // Todo title
+        const cardTitle = document.createElement('p');
+        cardTitle.textContent = todo.title;
+        cardContainer.appendChild(cardTitle);
+
+        // Todo description
+        const cardDesc = document.createElement('p');
+        cardDesc.textContent = todo.desc;
+        cardContainer.appendChild(cardDesc);
+
+        // Todo due date
+        const cardDue = document.createElement('p');
+        cardDue.textContent = todo.dueDate;
+        cardContainer.appendChild(cardDue);
+        
+        // Todo priority
+        const cardPriority = document.createElement('p');
+        cardPriority.textContent = todo.priority;
+        cardContainer.appendChild(cardPriority);
+
+        return cardContainer;
+    }
+
     renderProjects(projects)
     {
         for (let i = 0; i < projects.length; i++)
@@ -42,13 +71,14 @@ class DomManager
     {
         for (let i = 0; i < todos.length; i++)
         {
-            const testTodo = document.createElement('p');
-            testTodo.textContent = todos[i].title;
-            const something = document.createElement('p');
-            something.textContent = todos[i].desc;
+            // const testTodo = document.createElement('p');
+            // testTodo.textContent = todos[i].title;
+            // const something = document.createElement('p');
+            // something.textContent = todos[i].desc;
 
-            mainElem.appendChild(testTodo);
-            mainElem.appendChild(something);
+            // mainElem.appendChild(testTodo);
+            // mainElem.appendChild(something);
+            mainElem.appendChild(this.generateTodoCard(todos[i]));
         }
     }
 }
