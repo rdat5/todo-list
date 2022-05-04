a5f4e3a5a8a72e3b95c07f0fda73e651d410d52e
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-05-03 22:51:43 -0700

    Set up a default project on startup

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ domManager.initializeUI();
 
 // Add initial default project with default todo
 const userProjects = new UserProjects();
-userProjects.addProject(new Project());
-userProjects.projects[0].addTodo(new Todo());
-userProjects.projects[0].addTodo(new Todo("Other todo", "Other description"));
-userProjects.projects[0].addTodo(new Todo("Bruh", "Something or the other", "12/25/2022", "Low"));
-userProjects.addProject(new Project("Other Project"));
+userProjects.addProject(new Project("Default Project"));
+userProjects.projects[0].addTodo(new Todo("Make a todo list", 
+        "Add a todo item to your project", "2/5/1991", "Urgent"));
+userProjects.projects[0].todos[0].isDone = true;
+
+// userProjects.projects[0].addTodo(new Todo("Other todo", "Other description"));
+// userProjects.projects[0].addTodo(new Todo("Bruh", "Something or the other", "12/25/2022", "Low"));
+// userProjects.addProject(new Project("Other Project"));
 
 domManager.renderProjects(userProjects.projects);
 domManager.renderTodos(userProjects.projects[0].todos);
