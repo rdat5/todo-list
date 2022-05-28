c6c1fad0db8d219f9fd47dc2344a376e825c2f00
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-05-27 17:00:45 -0700

    Move localStorage check into data manager class

--- src/index.js
+++ src/index.js
@@ -9,32 +9,6 @@ console.log("Testing...");
 
 let userProjects;
 
-function storageAvailable(type) 
-{
-        var storage;
-        try {
-            storage = window[type];
-            var x = '__storage_test__';
-            storage.setItem(x, x);
-            storage.removeItem(x);
-            return true;
-        }
-        catch(e) {
-            return e instanceof DOMException && (
-                // everything except Firefox
-                e.code === 22 ||
-                // Firefox
-                e.code === 1014 ||
-                // test name field too, because code might not be present
-                // everything except Firefox
-                e.name === 'QuotaExceededError' ||
-                // Firefox
-                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-                // acknowledge QuotaExceededError only if there's something already stored
-                (storage && storage.length !== 0);
-        }
-}
-
 function createDefaultProject()
 {
         userProjects = new UserProjects();
@@ -47,7 +21,7 @@ function createDefaultProject()
         userProjects.projects[0].todos[0].isDone = true;
 }
 
-if (storageAvailable('localStorage'))
+if (DataManager.storageAvailable('localStorage'))
 {
         console.log("Storage is available");
 
